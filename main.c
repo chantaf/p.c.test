c6822eda408253fb2e104f8b812eccee1cedebd2
amalmtahri <mtahriamal0@gmail.com>
2021-01-22 11:25:27 +0100

    tttt

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int n,i;
+ int entier_binaire(int n){
+
+    return 0;
+}
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
+
 	int sous(int a,int b){
 		return 0;
 	}
-	
-	
-	
-	
+
+
+
+
 	return 0;
 }
