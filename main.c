3c00dd67adf84ba31fb3d706e7e05370b2433d2a
Othmane_Rhazlani <oghazlani0pgmail.com>
2021-01-22 11:19:37 +0100

    binaire to int

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//////// binaire to int
+int BinaireToInt(int binaire, int nombre) {
+
+   int result;
+   
+   
+   return result; 
+}
+
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
