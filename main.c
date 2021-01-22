5df6314a902fb15c8f81dc485249bd089ce00e9d
amalmtahri <mtahriamal0@gmail.com>
2021-01-22 15:40:49 +0100

    amal

--- main.c
+++ main.c
@@ -13,20 +13,34 @@ int BinaireToInt(int binaire, int nombre) {
    return result;
 }
 
-//entier_binaire
-int n,i;
-
- int entier_binaire(int n){
 
 
-    return 0;
-}
+   int T[100];
+    int i=0,j;
+    int n;
+    int entier_binaire(){
+    printf("donner un nombre :");
+    scanf("%d",&n);
+     while(n!=0)
+    {
+
+      T[i]=n%2;
+      printf("%d\n",T[i]);
+        n=n/2;
+        i++;
+    }
+    for(j=i-1;j>=0;j--){
+        printf("%d",T[j]);
+    }
+
+    return 0;}
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 
+//<<<<<<< HEAD
 	int sous(int a,int b){
 
 		int m1,c1,d1,u1,r1;
@@ -52,22 +66,72 @@ int main(int argc, char *argv[]) {
 
 
 
+=======
+	/*int sous(int a,int b){
+		 int s;
+		 s=a-b;
+
+		return s;*/
+
+	//CONVERTER dec binare
+/*	int tab[10], nbr, i;
+
+  printf("Entrez le nombre a convertir: ");
+  scanf("%d",&nbr);
+
+  for(i=0; nbr > 0; i++)
+  {
+    tab[i] = nbr%2;
+    nbr = nbr/2;
+  }
+
+  printf("\n Le nombre binaire est = ");
+
+  for(i=i-1; i >= 0; i--)
+  {
+    printf("%d",tab[i]);
+  }
+
+	*/
+
+
+
+
+
+
+
+//converter bin en dec
+int i,d=0;
+int t[4];
+
+for(i=4-1;i>=0;i--)
+{
+printf("\nDonnez le i EME  bit : ");
+scanf("%d",&t[i]);
+}
+
+for(i=0;i<4;i++)
+{
+
+d=d+t[i]*pow(2,i);
+}
+printf("\nLe nombre en decimal est : %d",d);
+
+>>>>>>> 755f99ae919f7d2d4ad4ad1713765ba079ef4f39
 		return 0;
 	}
 
 
 
-<<<<<<< HEAD
-=======
 
 
-//ss
+
+/*//ss
 int some(int bin1,int bin2){
 	return 0;
 }
 int main(int argc, char *argv[]) {
 
 
->>>>>>> 1be7993a9f6979993986b48aeb7196e0df4c5592
 	return 0;
-}
+}*/
