143da40946bfe846654b78bc4b75624cb8db18a6
chantaf <badr.chantaf@gmail.com>
2021-01-22 11:55:24 +0100

    aa

--- main.c
+++ main.c
@@ -26,31 +26,10 @@ int n,i;
 int main(int argc, char *argv[]) {
 
 	int sous(int a,int b){
+		 int s;
+		 s=a+b;
 		
-		int m1,c1,d1,u1,r1;
-		int m2,c2,d2,u2,r2;
-		
-		m1=a/1000;
-		r=a%1000;
-		c1=r1/100;
-		r=r1%100;
-		d1=r1/10;
-		r1=r1%10;
-		u1=r1;
-		
-		m2=b/1000;
-		r=b%1000;
-		c2=r2/100;
-		r2=r2%100;
-		d2=r2/10;
-		r2=r2%10;
-		u2=r2;
-		
-		
-		
-		
-		
-		return 0;
+		return s;
 	}
 
 
