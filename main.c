9c45f6ead055c3404772b513397388401fcc9526
chantaf <badr.chantaf@gmail.com>
2021-01-22 10:24:56 +0100

    testr

--- main.c
+++ main.c
@@ -4,5 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	int sous(int a,int b){
+		return 0;
+	}
+	
+	
+	
+	
 	return 0;
 }
