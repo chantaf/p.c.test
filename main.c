99ce768e8a505fcd682bd07a28072da253ba9ce3
soufianemj1 <elmajati.s@gmail.com>
2021-01-22 11:07:30 +0100

    plus

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+int some(int bin1,int bin2){
+	return 0;
+}
 int main(int argc, char *argv[]) {
+	
 	return 0;
 }
